75d6dea193b366fb1c6ebf704f4e4168de48428c
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-06-10 15:10:07 +0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /tmp
+apt-get -o Acquire::Check-Valid-Until=false update && apt-get -yq install wget libglib2.0-0 dnsmasq ca-certificates wondershaper
+file=uam-latest_amd64.deb
+rm $file
+wget --no-check-certificate  https://update.u.is/downloads/uam/linux/$file
+dpkg -i /tmp/$file
+cd /opt/uam/
+echo "[net]" >> /root/.uam/uam.ini
+container_ip="$(hostname -i)"
+echo "listens=[${container_ip}]:$2" >> /root/.uam/uam.ini
+./uam --pk $1 --http [0.0.0.0]:17099 --no-ui
