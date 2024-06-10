af6f61906825db9c16962d5178b9672810a6709b
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-06-10 15:12:27 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /tmp
-apt-get -o Acquire::Check-Valid-Until=false update && apt-get -yq install wget libglib2.0-0 dnsmasq ca-certificates wondershaper
+apt-get -y -o Acquire::Check-Valid-Until=false update && apt-get -yq install wget libglib2.0-0 dnsmasq ca-certificates wondershaper
 file=uam-latest_amd64.deb
 rm $file
 wget --no-check-certificate  https://update.u.is/downloads/uam/linux/$file
