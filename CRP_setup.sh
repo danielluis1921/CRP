af89837eff02c4e231ea8ebf36eb3a06ba777f73
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-06-12 14:45:56 +0700

    Update CRP_setup.sh

--- CRP_setup.sh
+++ CRP_setup.sh
@@ -7,7 +7,7 @@ sudo chmod 777 pre-install-uam.sh
 
 wget https://raw.githubusercontent.com/danielluis1921/CRP/main/generate-uam.sh
 sudo chmod 777 generate-uam.sh
-./generate-uam.sh 05D67162AC6E3701AC183D614F53C6A2D58D0BC063B1F16217969DD4C6263609 4
+./generate-uam.sh 05D67162AC6E3701AC183D614F53C6A2D58D0BC063B1F16217969DD4C6263609 2
 
 sudo apt-get -y install w3m w3m-img
 #watch -n 1 "curl -s 127.0.0.1:17100 | w3m -T text/html"
